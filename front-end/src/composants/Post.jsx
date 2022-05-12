eca42f366de3a7423767e25c8c0a9589a070021c
JACQUES sandra <ssephiha1905@gmail.com>
2022-05-12 19:59:00 +0200

    implementation ajouter  un post en front end

--- front-end/src/composants/Post.jsx
+++ front-end/src/composants/Post.jsx
@@ -1,22 +1,18 @@
-import Commentaire from "./Commentaire";
 
- function Post(props) {
-   const commentaires = [
-     'Commentaire 1',
-      'Commentaire 2',
-      'Commentaire 3',
-   ]
-   const texte = props.texte;
-     console.log(props);
+function Post(props) {
+  function supprimerPost() {
+    props.supprimerCePost(props.idPost);
+    console.log("supprimerPost");
+  }
   return (
     <div className="card 1">
-  <div className="card_image"> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
-  <div className="card_title title-white">
-    <p>{texte}</p>
-    <div className='commentaires'>{commentaires.map(com=> <Commentaire key={com} textCom={com}/>)}</div>
+      <div className="card_image"><img src="https://i.redd.it/b3esnz5ra34y.jpg" alt="post" /></div>
+      <div className="card_title title-black">
+        <p>{props.texte}</p>
+        <p className="id-post">{props.idPost}</p>
+        <button onClick={supprimerPost} className="btn-delete">Supprimer</button>
       </div>
-</div>
-      
+    </div>
   )
 }
 
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,30 +1,66 @@
 import {useEffect, useState} from 'react'
 import Post from '../composants/Post'
 
+
 /*utilisation de useEffect pour charger les données du backend au lancement de la page*/
 function Home() {
     const [listPost, setListPost] = useState([])
+    
+    const [inputContenu,setInputContenu]=useState('');          
+    
+function ajouterPost(){
+    //cloner le state listTaches
+    const listTemporaire = [...listPost];
+    //créer un nouveau post 
+    const newPost = {id:listTemporaire.length+1, contenu:inputContenu, };
+    //ajouter la nouveau post au tableau temporaire
+    listTemporaire.push(newPost);
+    //rafraichir le state
+    setListPost(listTemporaire);
+    //vider le input
+    setInputContenu('');
+   
+}
     useEffect(() => {
         fetch('http://localhost:3001/api/posts')
             .then(res => res.json())
             .then(data => setListPost(data))
     }, []) 
-    useEffect(() => {
-        fetch('http://localhost:3001/api/commentaire/id')
-            .then(res => res.json())
-            .then(data => setListPost(data))
-    }, []) 
+    
+    
+    
+    //supprimer un post
+    function supprimerUnPost(idPost) { 
+    //supprimer sur le backend
+    
+    fetch('http://localhost:3001/api/posts/' + idPost, {method: 'DELETE',})
+    .then(res =>{
+        if (res.status === 200){
+        //supprimer sur le frontend        
+        //cloner le state 
+        const listPostClone = [...listPost]
+        //filtrer les posts qui ne sont pas les mêmes que l'idPost
+        const listPostFiltre = listPostClone.filter(post => post.id !== idPost)
+        //raffraichir le state
+        setListPost(listPostFiltre)
+        }
+        else(
+            alert('erreur')
+        )        
+    })
+    
+    }
+
     return (
         <div>
             <h1>File d'actualités</h1>
-        <div className='cards-list'>
-                {listPost.map(p=><Post key= {p.id} texte={p.contenu}/>)}
-            </div>
-            
-            
+            <input type='text' placeholder="insérer le contenu du post" value={inputContenu} onChange={(e)=>setInputContenu(e.target.value)}/>
+                <button onClick={ajouterPost}>valider</button>
+                <div className='cards-list'>
+                {listPost.map(post=><Post key= {post.id} idPost={post.id} supprimerCePost={supprimerUnPost} texte={post.contenu}/>)}
+                </div>          
         </div>
     )
 }
 
-
-export default Home;
+export default Home
